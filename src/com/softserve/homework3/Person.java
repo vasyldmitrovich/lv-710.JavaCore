6e531e5787c4b5531d0af5477055279028cd8baa
lopatao <oleksandr.lopata@fuib.com>
2022-08-26 01:41:23 +0300

    input method for Person class

--- src/com/softserve/homework3/Person.java
+++ src/com/softserve/homework3/Person.java
@@ -1,21 +1,19 @@
 package com.softserve.homework3;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Person {
     private String firstName;
     private String lastName;
     private int birthYear;
 
-    public Person(String firstName, String lastName){
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public Person(String firstName, String lastName, int birthYear){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthYear = birthYear;
+    public Person() {
     }
 
     public String getFirstName() {
@@ -30,10 +28,21 @@ public class Person {
         return birthYear;
     }
 
-    public int getAge(){
+    public int getAge() {
         LocalDate current_date = LocalDate.now();
         int current_year = current_date.getYear();
         return current_year - birthYear;
     }
 
+    public void input(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first name ");
+        firstName = scanner.nextLine();
+        System.out.print("Enter last name ");
+        lastName = scanner.nextLine();
+        System.out.print("Enter birth year ");
+        birthYear = Integer.parseInt(scanner.nextLine());
+    }
+
+    
 }
