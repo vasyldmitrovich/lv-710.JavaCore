621e433f9c833c504e28999e526df3626e39d65e
lopatao <oleksandr.lopata@fuib.com>
2022-08-26 02:03:44 +0300

    creating Person from Main

--- src/com/softserve/homework3/Main.java
+++ src/com/softserve/homework3/Main.java
@@ -4,5 +4,12 @@ import com.softserve.homework3.Person;
 public class Main {
     public static void main(String[] args) {
 
+        Person person = new Person();
+        person.changeNames(null, "Oleksandr");
+        System.out.println(person.output());
+
+        person.input();
+        System.out.println(person.output());
+        System.out.println(person.getFirstName() + " age - " + person.getAge());
     }
 }
