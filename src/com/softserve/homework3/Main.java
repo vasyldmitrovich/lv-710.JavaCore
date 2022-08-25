7677c7091df66c3b8e9ef1dc3dde14be07cbdc38
lopatao <oleksandr.lopata@fuib.com>
2022-08-26 01:11:12 +0300

    Init homework files

---
+++ src/com/softserve/homework3/Main.java
@@ -0,0 +1,8 @@
+package com.softserve.homework3;
+import com.softserve.homework3.Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/softserve/homework3/Person.java
@@ -0,0 +1,4 @@
+package com.softserve.homework3;
+
+public class Person {
+}
