29c4451e0eb4c8ae306e6b2507c9b26e7e876af7
lopatao <oleksandr.lopata@fuib.com>
2022-08-26 01:31:16 +0300

    Constructors and getters for Person class

--- src/com/softserve/homework3/Person.java
+++ src/com/softserve/homework3/Person.java
@@ -1,4 +1,34 @@
 package com.softserve.homework3;
 
+import java.time.LocalDate;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int birthYear){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+
 }
