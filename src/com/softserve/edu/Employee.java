a44f93e3a0f0959d3600d6a636de7965b7a55f12
YuriiKoval96 <yurakoval96@gmail.com>
2022-08-25 22:42:30 +0300

    Added class Employee to the project. added three private fields: name, rate and hours; added static field totalSum; added properties for access to these fields; added default constructor, constructor with 2 parameters (name and rate) and constructor with 3 parameters; added methods: getSalary() - to calculate the salary of person (rate * hours)                toString() - to output information about employee                changeRate(int rate) - to change the rate of some employee and recalculate his salary                getBonuses() – to calculate 10% from salary Created 3 objects of Employee type. Displayed the total salary of all workers to screen

--- src/com/softserve/edu/Employee.java
+++ src/com/softserve/edu/Employee.java
@@ -21,12 +21,54 @@ public class Employee {
         this.hours = hours;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setRate(long rate) {
+        this.rate = rate;
+    }
+
+    public long getRate() {
+        return this.rate;
+    }
+
+    public void setHours(long hours) {
+        this.hours = hours;
+    }
+
+    public long getHours() {
+        return this.hours;
+    }
+
     public long getSalary() {
+        totalSum += this.rate * this.hours;
         return this.rate * this.hours;
     }
 
-    public String methodToString() {
-        return "Name of employee is " + this.name + ", his rate is " + this.rate + " USD/hour," +
-                " he worked " + this.hours + " hours.";
+    @Override
+    public String toString() {
+        return getName() + "'s rate is " + getRate() + " USD/hour," +
+                " he worked " + getHours() + " hours.";
+    }
+
+    public long changeRate(long rate){
+        totalSum -= (this.rate * this.hours);
+        this.rate = rate;
+        getSalary();
+        return this.rate;
+    }
+
+    public long getBonuses() {
+        totalSum += this.rate * this.hours / 10;
+        return this.rate * this.hours / 10;
+    }
+
+    public static long getTotalSum() {
+        return totalSum;
     }
 }
--- src/com/softserve/edu/Main.java
+++ src/com/softserve/edu/Main.java
@@ -2,8 +2,20 @@ package com.softserve.edu;
 
 public class Main {
     public static void main(String[] args) {
-        Employee yurii = new Employee("Yurii", 100, 40);
-        System.out.println(yurii.methodToString());
+        Employee yurii = new Employee("Yurii", 10, 172);
+        Employee stepan = new Employee("Stepan", 10, 164);
+        Employee serhii = new Employee("Serhii", 10, 156);
+        System.out.println(yurii.toString());
+        System.out.println(yurii.getName() + "'s salary in this month is " + yurii.getSalary() + " USD/hour");
+        yurii.changeRate(11);
+        System.out.println("But he worked well and we decide to increase his rate to " + yurii.getRate() + " USD/hour");
+        System.out.println("Also Yurii gets bonus " + (yurii.getBonuses()) + " USD");
+        System.out.println("So, final " + yurii.getName() + "'s salary in this month is "
+                + Employee.getTotalSum() + " USD");
+        System.out.println(stepan.toString());
+        System.out.println(stepan.getName() + "'s salary in this month is " + stepan.getSalary() + " USD");
+        System.out.println(serhii.getName() + "'s salary in this month is " + serhii.getSalary() + " USD");
+        System.out.println("Our team's salary expenses in this month are " + Employee.getTotalSum() + " USD");
 
     }
 }
