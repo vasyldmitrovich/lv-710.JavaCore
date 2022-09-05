c1f3073e8726f26cedff2372f30ea36fc2fbfe1b
Hromylo <grom81861@gmail.com>
2022-09-05 17:08:49 +0300

    Change to new file

---
+++ src/com/softserve/edu04/Hmodd.java
@@ -0,0 +1,30 @@
+package edu04;
+
+import java.util.Scanner;
+
+public class Hmodd 	{
+public static void main(String[] args) {
+	countOdd();
+}
+
+private static void countOdd() {
+	 Scanner in= new Scanner (System.in);
+	   
+	  int oddNumbers = 0;
+  
+  for(int i= 1;i<=3;i++)
+  {
+      System.out.println("Enter 3 numbers: ");
+      int numbers= in.nextInt();
+
+      if(numbers %2==1)
+      {
+          oddNumbers++;
+      }
+  }
+  System.out.println("There are: "+oddNumbers+" odd numbers");
+  
+
+   }
+	
+}
