bae1a7ae38cdb7f2ec4b70ad261bd2c9d64da938
oksana <gafenlofn@gmail.com>
2022-09-07 23:34:45 +0300

    Task2 edu04 added

---
+++ src/com/softserve/edu04/DayOfTheWeek.java
@@ -0,0 +1,58 @@
+package com.softserve.edu04;
+
+public enum DayOfTheWeek {
+    MONDAY(1, "Понеділок", "Monday", "Lunas"),
+    TUESDAY(2, "Вівторок", "Tuesday", "Martes"),
+    WEDNESDAY(3, "Середа", "Wednesday", "Miercoles"),
+    THURSDAY(4, "Четвер", "Thursday", "Jueves"),
+    FRIDAY(5, "Пятниця", "Friday", "Viernes"),
+    SATURDAY(6, "Субота", "Saturday", "Con"),
+    SUNDAY(7, "Неділя", "Sunday", "Domingo");
+
+    private int dayOfTheWeekNumber;
+    private String ua;
+    private String en;
+    private String es;
+
+    DayOfTheWeek(int dayOfTheWeekNumber, String ua, String en, String es) {
+        this.dayOfTheWeekNumber = dayOfTheWeekNumber;
+        this.ua = ua;
+        this.en = en;
+        this.es = es;
+    }
+
+    public String getUa() {
+        return ua;
+    }
+
+    public String getEn() {
+        return en;
+    }
+
+    public String getEs() {
+        return es;
+    }
+
+
+    public static DayOfTheWeek getByNumber(int dayOfTheWeekNumber) {
+        switch (dayOfTheWeekNumber) {
+            case 1:
+                return MONDAY;
+            case 2:
+                return TUESDAY;
+            case 3:
+                return WEDNESDAY;
+            case 4:
+                return THURSDAY;
+            case 5:
+                return FRIDAY;
+            case 6:
+                return SATURDAY;
+            case 7:
+                return SUNDAY;
+            default:
+                return null;
+        }
+
+    }
+}
---
+++ src/com/softserve/edu04/Task2.java
@@ -0,0 +1,13 @@
+package com.softserve.edu04;
+
+public class Task2 {
+    public static void main(String[] args) {
+        DayOfTheWeek dayOfTheWeek = DayOfTheWeek.getByNumber(5);
+        System.out.println(dayOfTheWeek.getEn());
+        System.out.println(dayOfTheWeek.getEs());
+        System.out.println(dayOfTheWeek.getUa());
+
+    }
+
+
+    }
