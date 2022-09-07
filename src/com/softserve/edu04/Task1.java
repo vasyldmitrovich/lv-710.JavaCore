108c89c779803d4b27d1608d4b13f7c7190174ef
oksana <gafenlofn@gmail.com>
2022-09-07 22:42:01 +0300

    Task 1 eduo4 added

---
+++ src/com/softserve/edu04/Task1.java
@@ -0,0 +1,24 @@
+package com.softserve.edu04;
+
+public class Task1 {
+
+    private static final String MESSAGE = "Number %s is odd: %s";
+
+    public static void main(String[] args) {
+
+
+        int a = 453;
+        int b = 344;
+        int c = 567;
+
+        System.out.println(MESSAGE.formatted(a, isOdd(a)));
+        System.out.println(MESSAGE.formatted(b, isOdd(b)));
+        System.out.println(MESSAGE.formatted(c, isOdd(c)));
+
+    }
+
+    static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+}
