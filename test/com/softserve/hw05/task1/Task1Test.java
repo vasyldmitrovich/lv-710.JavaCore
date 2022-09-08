4f3259db9accbeabb6d8eb2ad607d9beacb938b1
Sehii <dolivo.serg@gmail.com>
2022-09-08 11:56:38 +0300

    pom.xml

---
+++ test/com/softserve/hw05/task1/Task1Test.java
@@ -0,0 +1,12 @@
+package com.softserve.hw05.task1;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class Task1Test extends TestCase {
+
+    @Test
+    public void testMain() {
+        
+    }
+}
