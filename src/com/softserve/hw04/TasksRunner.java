f9d572f62f6d4ce7f60d89175df99511ef07c9ef
oksana <gafenlofn@gmail.com>
2022-09-10 18:48:01 +0300

    added task runner

---
+++ src/com/softserve/hw04/TasksRunner.java
@@ -0,0 +1,25 @@
+package com.softserve.hw04;
+
+public class TasksRunner {
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+
+    }
+
+    public static void task1() {
+
+    }
+    public static void task2() {
+
+    }
+    public static void task3() {
+
+    }
+    public static void task4() {
+
+    }
+}
