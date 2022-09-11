c3c8ba792253e8b8de701d694271c41ff2375a24
VadimVadimi4 <113105963+johnlenon9405@users.noreply.github.com>
2022-09-11 14:07:52 +0300

    Update task2.java

--- src/com/softserve/edu2/task2.java
+++ src/com/softserve/edu2/task2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class task2 {
-    public static void quest(String[] args){
+public class Main {
+    public static void main(String[] args){
         System.out.println("Hi, what's your name ?");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
