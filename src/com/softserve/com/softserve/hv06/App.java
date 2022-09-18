5c3ffadfed3516eda018cd4b9d3269be088580b1
Hromylo <grom81861@gmail.com>
2022-09-18 14:27:17 +0300

    Hv 06 birds

---
+++ src/com/softserve/com/softserve/hv06/App.java
@@ -0,0 +1,12 @@
+package com.softserve.hv06;
+
+import com.softserve.hv06.Tasks.TaskBirds;
+
+public class App {
+
+	public static void main(String[] args) {
+		TaskBirds.runBirds();
+
+	}
+
+}
---
+++ src/com/softserve/com/softserve/hv06/Classes/FlyingBird.java
@@ -0,0 +1,11 @@
+package com.softserve.hv06.Classes;
+
+public class FlyingBird extends Bird {
+
+	public void fly() {
+		
+		System.out.println("The bird flying with" + feathers +" "+ layEggs);
+
+	}
+
+}
