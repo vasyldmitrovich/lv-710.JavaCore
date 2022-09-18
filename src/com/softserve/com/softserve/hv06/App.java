4fa86918bcee2637ad55c6e9370fa637fc9c77b6
Hromylo <grom81861@gmail.com>
2022-09-18 15:42:33 +0300

    All hv 06

--- src/com/softserve/com/softserve/hv06/App.java
+++ src/com/softserve/com/softserve/hv06/App.java
@@ -1,11 +1,13 @@
 package com.softserve.hv06;
 
 import com.softserve.hv06.Tasks.TaskBirds;
+import com.softserve.hv06.Tasks.TaskDeveloper;
 
 public class App {
 
 	public static void main(String[] args) {
 		TaskBirds.runBirds();
+		TaskDeveloper.taskDeveloper();
 
 	}
 
