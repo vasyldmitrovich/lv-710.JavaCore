d96a37ae7be3e24425477eae96f901fa32e192ed
ihomziak <homziaki@ae.com>
2022-10-01 15:56:21 +0300

    hw05

--- src/main/java/com/softserve/hw05/Calculations.java
+++ src/main/java/com/softserve/hw05/Calculations.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 
 public class Calculations {
 
-    public void isPositiveOrNegativeInt (int [] value) {
-        for (int i = 0 ; i < value.length ; i++) {
-            if(value[i] > -1) {
-                System.out.println( value[i] + " - is a positive Integer");
-            } else if(value[i] <= -1) {
-                System.out.println( value[i] + " - is a negative Integer");
+    public void isPositiveOrNegativeInt(int[] value) {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] > -1) {
+                System.out.println(value[i] + " - is a positive Integer");
+            } else if (value[i] <= -1) {
+                System.out.println(value[i] + " - is a negative Integer");
             }
         }
     }
 
     public int sumOfPositiveNumbers(int[] value) {
         int positiveIntSum = 0;
-        for (int i = 0 ; i < value.length ; i++) {
+        for (int i = 0; i < value.length; i++) {
             if (value[i] > -1) {
                 positiveIntSum = value[i] + positiveIntSum;
             }
@@ -56,5 +56,57 @@ public class Calculations {
         System.out.println(positiveInt > negativeInt ? "There are more positive int = " + positiveInt :
                 "There are more negative  int = " + negativeInt);
         return Math.max(positiveInt, negativeInt);
+    }
+
+    public void firstFivePosNum(int[] numbers) {
+        int sum = 0;
+        int count = 0;
+        for (Integer number : numbers) {
+            if (number >= 0) {
+                sum = number + sum;
+                count++;
+                if (count == 5) {
+                    break;
+                }
+            }
+        }
+        System.out.println("Sum of first 5th positive numbers is: " + sum);
+    }
+
+    public void positionOfSecondPositiveNum(int[] numbers) {
+        int count = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > 0) {
+                count++;
+                if (count == 2) {
+                    System.out.println("Second positive number have an index equal: " + numbers[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    public int indexOfSmallestValue(int[] array) {
+        int index = 0;
+        int min = array[index];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] <= min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        System.out.println("The smallest value is: " + min + " with index: " + index);
+        return index;
+    }
+
+    public int sumOfEvenNumbers(int[] numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            if (number % 2 == 0) {
+                sum = sum + number;
+            }
+        }
+        System.out.println("Sum of all even numbers is equal: " + sum);
+        return sum;
     }
 }
--- src/main/java/com/softserve/hw05/Main.java
+++ src/main/java/com/softserve/hw05/Main.java
@@ -1,4 +1,46 @@
 package com.softserve.hw05;
 
+import java.util.Arrays;
+
 public class Main {
+
+    static Calculations calc = new Calculations();
+    private static final int[] digits = {-3, -5, 4, 6, 9, -8, 0, 2, 1, -10};
+
+    public static void main(String[] args) {
+
+
+        task2();
+        task3();
+
+//        calc.isPositiveOrNegativeInt(digits);
+
+
+//        calc.sumOfPositiveNumbers(digits);
+//
+//        calc.amountOfNegativeNumbers(digits);
+//
+//        calc.maxInt(digits);
+//
+//        calc.moreNegativeOrPositive(digits);
+    }
+
+    public static void task1() {
+    }
+
+    public static void task2() {
+        calc.firstFivePosNum(digits);
+    }
+
+    public static void task3() {
+        calc.positionOfSecondPositiveNum(digits);
+        calc.indexOfSmallestValue(digits);
+        calc.sumOfEvenNumbers(digits);
+    }
+
+    public static void task4() {
+    }
+
+    public static void task5() {
+    }
 }
