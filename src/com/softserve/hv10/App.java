88416b39c397114ea67361849ef7b719562a8470
Hromylo <grom81861@gmail.com>
2022-10-03 22:36:46 +0300

    hv 10 3 tasks

--- src/com/softserve/hv10/App.java
+++ src/com/softserve/hv10/App.java
@@ -4,7 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		com.softserve.hv10.Classes.FiveFWords.fiveWords();
+		//com.softserve.hv10.Classes.FiveFWords.fiveWords();
+		//com.softserve.hv10.Classes.Spaces.spaces();
+		com.softserve.hv10.Classes.Dollars.dollars();
 	}
 
 }
