72939e15bc16f7f14bb2c298243a8c69f588c244
Hromylo <grom81861@gmail.com>
2022-10-07 11:24:12 +0300

    add intutMismatchxception

--- src/com/softserve/hv11/Classes/StartEnd.java
+++ src/com/softserve/hv11/Classes/StartEnd.java
@@ -32,53 +32,28 @@ public class StartEnd {
 		Scanner scc=new Scanner(System.in);
 		System.out.println("Enter the elements of the array: ");  
 		for(int i=0; i<10; i++){  
-			array[i]=scc.nextInt();
+			 try
+			    {
+				 array[i]=scc.nextInt();
+			    }
+			    catch (InputMismatchException exception)
+			    {
+			        System.out.println("Integers only, please."); break;
+			    }
+			
 			try  {
 				if (array[i]<start || array[i]> end) 
 				 throw new ArithmeticException();
 				}
 				catch (ArithmeticException b) {
-					System.out.println("out of gates");
+					System.out.println("out of gates"); break;
 				}
-			catch(InputMismatchException b){
-				System.out.println("Invalid input");
-			}
 			
 					finally { System.out.println((i+1) + " element= "+ array[i]);
 					}
 		}
-			}
-			
-			
-		
-	
-		
-		
-		
-		
-			
-		
-		/*Scanner input = new Scanner(System.in);
-	    System.out.println ("Insert start");
-		int start = input.nextInt();
-		System.out.println ("Insert end");
-		int end = input.nextInt();
-		input.close();
-		
-        Scanner sc=new Scanner(System.in);
-		
-		int[] array = new int[10];  
-		System.out.println("Enter the elements of the array: ");  
-		for(int i=0; i<10; i++)  
-		{  
-		array[i]=sc.nextInt();  
-		sc.close();*/
-		//for(int i=0; i<10; i++)  
-		
-		
-		
 	}
-	
+}
 	
 		
 		
