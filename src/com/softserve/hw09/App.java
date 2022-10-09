9803db012aec7a7678299dded0a64fc4c95ee204
YuriiKoval96 <yurakoval96@gmail.com>
2022-10-09 15:49:51 +0300

    updated hw09

--- src/com/softserve/hw09/App.java
+++ src/com/softserve/hw09/App.java
@@ -4,9 +4,8 @@ public class App {
 
     public static void main(String[] args){
 
-        //com.softserve.hw09.tasks.Task1.runTask1();
-        //com.softserve.hw09.tasks.Task2.runTask2();
-        com.softserve.hw09.tasks.Task3.runTask3();
+
+
     }
 
 }
