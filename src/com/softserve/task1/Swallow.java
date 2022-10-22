75f152cc770dd8966fce20dd9b2e5a0ccfdf110a
Alina Ladur <78309279+alina-ladur@users.noreply.github.com>
2022-10-22 23:25:32 +0300

    Delete src/com/softserve/task1 directory

--- src/com/softserve/task1/Swallow.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.task1;
-
-public class Swallow extends Flying_Bird{
-    public Swallow() {
-        this.setType("Swallow");
-        this.setFeathers("brown, dark blue and white");
-        this.setLayEggs(true);
-    }
-}
